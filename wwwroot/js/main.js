b1dcfe291afb4b5d93605861332c6444096737ab
ailo21 <alex92ailo@gmail.com>
2020-03-28 15:13:11 +0300

    пример стилизации страницы проекта

---
+++ wwwroot/js/main.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+    //груглый график
+
+    $('.graph_doughnut').each(function () {
+        var $id_attr = $(this).attr('id');
+        var $obj = document.getElementById($id_attr);
+        var circle = Circles.create({
+            id: $obj.id,
+            value: $obj.getAttribute('data-proc'),
+            radius: 60,//getWidth(),
+            width: 32,
+            duration: 750,
+            text: function (value) { return value + '%'; },
+            textClass: 'circles-text',
+            colors: ['#e6e6e6', $obj.getAttribute('data-color')],//colors[i - 1],
+            styleText: false
+        });
+        console.log(circle);
+
+    });
+});
